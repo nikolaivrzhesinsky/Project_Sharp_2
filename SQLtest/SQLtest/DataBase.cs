77ee30beca7880bf24d55692a4bc565f6971637e
nikolaivrzhesinsky <63013424+nikolaivrzhesinsky@users.noreply.github.com>
2021-11-28 13:27:44 +0300

    -

---
+++ SQLtest/SQLtest/DataBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+
+namespace SQLtest
+{
+    public class DataBase
+    {
+        static string host = "localhost";
+        static int port = 3306;
+        static string database = "users_tab";
+        static string username = "root";
+        static string password = "8616";
+
+       
+        public static MySqlConnection GetDB()
+        {
+             String connString = "Server=" + host + ";Database=" + database
+             + ";port=" + port + ";User Id=" + username + ";password=" + password;
+            MySqlConnection conn = new MySqlConnection(connString);
+            return conn;
+        }
+
+        
+
+    }
+}
--- SQLtest/SQLtest/Program.cs
+++ SQLtest/SQLtest/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
+
 
 namespace SQLtest
 {
     class Program
     {
+        DataBase obj1 = new DataBase();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Getting Connection ...");
+            MySqlConnection connection = DataBase.GetDB();
+           
+
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+                connection.Open();
+
+                Console.WriteLine("Connection successful!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            Console.Read();
         }
     }
 }
