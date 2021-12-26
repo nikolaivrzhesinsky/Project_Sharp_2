6f1418ac334290922ad818d42840c78a4c22d777
Ruslan0901 <71920118+Ruslan0901@users.noreply.github.com>
2021-12-26 05:55:12 +0300

    Откорректировал файл

--- CuurseWork/DataLayer_BuisnessLayer/Matrix_compression.cs
+++ CuurseWork/DataLayer_BuisnessLayer/Matrix_compression.cs
@@ -14,7 +14,6 @@ namespace DataLayer_BuisnessLayer
         public List<string> colName;
         public Matrix_compression(int Row, int Col)
         {
-            //excep на Row, Col <0
             matrix = new double[Row, Col];
             row = Row;
             col = Col;
@@ -69,18 +68,6 @@ namespace DataLayer_BuisnessLayer
                 this.matrix[Row, Col] = value;
             }
         }
-        public static Matrix_compression Transpo(Matrix_compression baseMatrix)
-        {
-            var transpoMatrix = new Matrix_compression(baseMatrix.col, baseMatrix.row);
-            for (int i = 0; i < transpoMatrix.col; i++)
-            {
-                for (int j = 0; j < transpoMatrix.row; j++)
-                {
-                    transpoMatrix.matrix[j, i] = baseMatrix.matrix[i, j];
-                }
-            }
-            return transpoMatrix;
-        }
         public static Matrix_compression MultiMatrix(Matrix_compression firstMatrix, Matrix_compression secondMatrix)
         {
             //excep на firstMatrix.col != secondMatrix.row
@@ -97,29 +84,23 @@ namespace DataLayer_BuisnessLayer
             }
             return resMatrix;
         }
-        public static void MultiNumber(Matrix_compression baseMatrix, int num)
+        public static Matrix_compression MultiForDist(Matrix_compression first, Matrix_compression second)
         {
-            for (int i = 0; i < baseMatrix.row; i++)
+            var res = new Matrix_compression(first.row, second.col);
+            for (int i = 0; i < res.row; i++)
             {
-                for (int j = 0; j < baseMatrix.col; j++)
+                for (int k = 0; k < res.col; k++)
                 {
-                    baseMatrix.matrix[i, j] *= num;
+                    for (int j = 0; j < first.col; j++)
+                    {
+                        res[i, k] += (first[i, j] - second[j, k]) * (first[i, j] - second[j, k]);
+                    }
                 }
             }
+
+            return res;
         }
-        public static Matrix_compression SumMatrix(Matrix_compression firstMatrix, Matrix_compression secondMatrix)
-        {
-            //excep на firstMatrix.row != secondMatrix.row or (col != col)
-            var resMatrix = new Matrix_compression(firstMatrix.row, secondMatrix.col);
-            for (int i = 0; i < firstMatrix.row; i++)
-            {
-                for (int j = 0; j < firstMatrix.col; j++)
-                {
-                    resMatrix[i, j] = firstMatrix[i, j] + secondMatrix[i, j];
-                }
-            }
-            return resMatrix;
-        }
+      
     }
     public class Connections
     {
@@ -145,13 +126,7 @@ namespace DataLayer_BuisnessLayer
                     resMatrix[i, k] = 0;
                 }
             }
-            //Console.WriteLine("txtmat");
-            //Print(wordMatrix);
-            //Console.WriteLine("wordmat");
-            //Print(txtMatrix);
-            resMatrix = MultiForDist(wordMatrix, txtMatrix);
-            //resMatrix = Matrix.MultiMatrix(wordMatrix, normalizedMatrix);
-            //resMatrix = Matrix.MultiMatrix(resMatrix, txtMatrix);
+            resMatrix = Matrix_compression.MultiForDist(wordMatrix, txtMatrix);
             for (int i = 0; i < resMatrix.row; i++)
             {
                 for (int k = 0; k < resMatrix.col; k++)
@@ -159,23 +134,8 @@ namespace DataLayer_BuisnessLayer
                     resMatrix[i, k] = Math.Round(resMatrix[i, k], 6);
                 }
             }
-            //Console.WriteLine("res mat");
-            //Print(resMatrix);
             var resTuple = new List<Tuple<int, string>>();
             Tuple<int, string> tmp;
-            //for (int i = 0; i < resMatrix.col; i++)
-            //{
-            //    int id = 0;
-            //    for (int k = 1; k < resMatrix.row; k++)
-            //    {
-            //        if (resMatrix[id, i] < resMatrix[k, i])
-            //        {
-            //            id = k;
-            //        }
-            //    }
-            //    tmp = new Tuple<int, string>(i + 1, wordMatrix.colName[id]);
-            //    resTuple.Add(tmp);
-            //}
             for (int i = 0; i < resMatrix.col; i++)
             {
                 int id = 0;
@@ -192,22 +152,7 @@ namespace DataLayer_BuisnessLayer
             return resTuple;
         }
 
-        public static Matrix_compression MultiForDist(Matrix_compression first, Matrix_compression second)
-        {
-            var res = new Matrix_compression(first.row, second.col);
-            for (int i = 0; i < res.row; i++)
-            {
-                for (int k = 0; k < res.col; k++)
-                {
-                    for (int j = 0; j < first.col; j++)
-                    {
-                        res[i, k] += (first[i, j] - second[j, k]) * (first[i, j] - second[j, k]);
-                    }
-                }
-            }
 
-            return res;
-        }
         public static List<Tuple<int, string>> CreateResult(double[,] U, double[,] S, double[,] Vt, List<string> word_List)
         {
             var maceMatrix = new Matrix_compression(U, word_List);
